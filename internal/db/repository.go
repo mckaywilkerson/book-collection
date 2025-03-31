28c7027af1bc9590abf6bbc4082b382c0c171b03
McKay Wilkerson <38505872+mckaywilkerson@users.noreply.github.com>
2025-03-31 15:31:28 -0600

    db.GetAllBooks

--- internal/db/repository.go
+++ internal/db/repository.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/mckaywilkerson/book-collection/internal/models"
 )
 
 // ConnectDB - connects to a given postgres database, via pgxpool (for simultaneous connections)
@@ -24,6 +25,34 @@ func ConnectDB(dsn string) (*pgxpool.Pool, error) {
 	return dbPool, nil
 }
 
-func GetBooks(conn *pgxpool.Conn) ([]Book, error) {
+// GetAllBooks - returns list of all books in the database
+func GetAllBooks(dbPool *pgxpool.Pool) ([]models.Book, error) {
+	conn, err := dbPool.Acquire(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Release()
+
+	statement := "SELECT id, title, author, publication_year FROM books"
+
+	pgxRows, err := conn.Query(context.Background(), statement)
+	if err != nil {
+		return nil, err
+	}
+	defer pgxRows.Close()
+
+	var books []models.Book
+	for pgxRows.Next() {
+		var singleBook models.Book
+		if err := pgxRows.Scan(&singleBook.ID, &singleBook.Title, &singleBook.Author, &singleBook.PublicationYear); err != nil {
+			return nil, err
+		}
+		books = append(books, singleBook)
+	}
+	if err := pgxRows.Err(); err != nil {
+		return nil, err
+	}
+
+	return books, nil
 
 }
---
+++ internal/handlers/book_handlers.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func HandleGetAllBooks(dbPool *pgxpool.Pool) http.HandlerFunc {
+
+}
