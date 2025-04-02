11268626bc5f4cc420e1dd934307264033a743b6
McKay Wilkerson <38505872+mckaywilkerson@users.noreply.github.com>
2025-04-02 17:39:32 -0600

    Incorporate docker for db testing

---
+++ internal/db/repository_test.go
@@ -0,0 +1,73 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "latest",
+		Env: []string{
+			"POSTGRES_PASSWORD=secret",
+			"POSTGRES_USER=user_name",
+			"POSTGRES_DB=books",
+			"listen_addresses='*'",
+		},
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseURL := fmt.Sprintf("postgres://user_name:secret@%s/books?sslmode=disable", hostAndPort)
+
+	log.Println("Connecting to database on url:", databaseURL)
+
+	resource.Expire(120)
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	pool.MaxWait = 120 * time.Second
+	err = pool.Retry(func() error {
+		db, err := sql.Open("pgx", databaseURL)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	})
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	defer func() {
+		err = pool.Purge(resource)
+		if err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+	}()
+
+	m.Run()
+}
